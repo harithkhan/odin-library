9694f0c4b8b75fbbaaf6d245e789b1d5da28ea8b
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-01-05 16:05:37 +0800

    feat: add new books to default display library

--- javascript.js
+++ javascript.js
@@ -14,10 +14,32 @@ function addBookToLibrary(bookToAdd) {
     myLibrary.push(bookToAdd);
 }
 
-let book1 = new Book("How to Die", "H. Khan", 69, "not read");
-let book2 = new Book("How to Die faster", "H. Khan", 42, "read")
+let book1 = new Book("Emma", "Jane Austen", 495, "read");
+let book2 = new Book("Emma", "Pride and Prejudice", 274, "not read");
+let book3 = new Book("Heidi Shamsuddin", "Nusantara, A Sea of Tales", 223, "not read");
+let book4 = new Book("Slaying Dragons", "Kristian Bell", 282, "not read");
+let book5 = new Book("L. Varty & L. H. Yim", "Hong Kong Slang", 115, "not read");
+let book6 = new Book("Levy Rozman", "How To Win At Chess", 361, "read");
+let book7 = new Book("Fyodor Dostoevsky", "Crime and Punishment", 642, "read");
+let book8 = new Book("Gabot Mate", "The Myth of Normal", 563, "read");
+let book9 = new Book("Alan Watts", "Nature, Man and Woman", 187, "not read");
+let book10 = new Book("Alex Hormozi", "$100m Offers", 231, "read");
+let book11 = new Book("Alex Hormozi", "$100m Leads", 398, "read");
+let book12 = new Book("Vadim Zealand", "Tufti The Priestess", 167, "read");
+
+// Add default display books
 
 addBookToLibrary(book1);
 addBookToLibrary(book2);
+addBookToLibrary(book3);
+addBookToLibrary(book4);
+addBookToLibrary(book5);
+addBookToLibrary(book6);
+addBookToLibrary(book7);
+addBookToLibrary(book8);
+addBookToLibrary(book9);
+addBookToLibrary(book10);
+addBookToLibrary(book11);
+addBookToLibrary(book12);
 console.log(myLibrary);
 
