6f3cc0d430c32097ce28c62ce6ff50d2f4181943
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-01-05 18:50:37 +0800

    feat: add image path to displayLibrary function

--- javascript.js
+++ javascript.js
@@ -17,18 +17,18 @@ function addBookToLibrary(bookToAdd) {
 
 // Add default books to myLibary
 
-let book1 = new Book("Emma", "Jane Austen", 495, "read", "img/books/emma");
-let book2 = new Book("Emma", "Pride and Prejudice", 274, "not read", "img/books/pride");
-let book3 = new Book("Heidi Shamsuddin", "Nusantara, A Sea of Tales", 223, "not read", "img/books/nusantara");
-let book4 = new Book("The Psychology of Slaying Dragons", "Kristian Bell", 282, "not read", "img/books/dragons");
-let book5 = new Book("L. Varty & L. H. Yim", "Hong Kong Slang", 115, "not read", "img/books/hkslang");
-let book6 = new Book("Levy Rozman", "How To Win At Chess", 361, "read", "img/books/chess");
-let book7 = new Book("Fyodor Dostoevsky", "Crime and Punishment", 642, "read", "img/books/crime");
-let book8 = new Book("Gabot Mate", "The Myth of Normal", 563, "read", "img/books/mythofnormal");
-let book9 = new Book("Alan Watts", "Nature, Man and Woman", 187, "not read", "img/books/nature");
-let book10 = new Book("Alex Hormozi", "$100m Offers", 231, "read", "img/books/100moffers");
-let book11 = new Book("Alex Hormozi", "$100m Leads", 398, "read", "img/books/100mleads");
-let book12 = new Book("Vadim Zealand", "Tufti The Priestess", 167, "read", "img/books/tufti");
+let book1 = new Book("Emma", "Jane Austen", 495, "read", "img/books/emma.png");
+let book2 = new Book("Emma", "Pride and Prejudice", 274, "not read", "img/books/pride.png");
+let book3 = new Book("Heidi Shamsuddin", "Nusantara, A Sea of Tales", 223, "not read", "img/books/nusantara.png");
+let book4 = new Book("The Psychology of Slaying Dragons", "Kristian Bell", 282, "not read", "img/books/dragons.png");
+let book5 = new Book("L. Varty & L. H. Yim", "Hong Kong Slang", 115, "not read", "img/books/hkslang.png");
+let book6 = new Book("Levy Rozman", "How To Win At Chess", 361, "read", "img/books/chess.png");
+let book7 = new Book("Fyodor Dostoevsky", "Crime and Punishment", 642, "read", "img/books/crime.png");
+let book8 = new Book("Gabot Mate", "The Myth of Normal", 563, "read", "img/books/mythofnormal.png");
+let book9 = new Book("Alan Watts", "Nature, Man and Woman", 187, "not read", "img/books/nature.png");
+let book10 = new Book("Alex Hormozi", "$100m Offers", 231, "read", "img/books/100moffers.png");
+let book11 = new Book("Alex Hormozi", "$100m Leads", 398, "read", "img/books/100mleads.png");
+let book12 = new Book("Vadim Zealand", "Tufti The Priestess", 167, "read", "img/books/tufti.png");
 
 // Add default display books
 
@@ -52,8 +52,16 @@ const library = document.querySelector(".library");
 function displayLibrary() {
     library.innerHTML = ""; // Clear existing library
     for (let book of myLibrary) {
-        const bookCard = document.createElement("div");
+        let bookCard = document.createElement("div");
         bookCard.className = "book-card";
+        let bookImg = document.createElement("img");
+        bookImg.setAttribute("src", `${book.imgPath}`);
+        bookImg.setAttribute("class", "book-icon")
+        let bookTitle = document.createElement("div");
+        bookTitle.className = "book-title";
+        bookTitle.textContent = `${book.title}`;
+        bookCard.appendChild(bookImg);
+        bookCard.appendChild(bookTitle);
         library.appendChild(bookCard);
     }
 };
