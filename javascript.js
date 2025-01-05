7fbd51de5a33bb330ba48042162e8b36742ef969
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-01-05 16:46:22 +0800

    feat: add image file path to default books

--- javascript.js
+++ javascript.js
@@ -1,10 +1,11 @@
 const myLibrary = [];
 
-function Book(title, author, pages, isRead) {
+function Book(title, author, pages, isRead, imgPath) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.isRead = isRead;
+    this.imgPath = imgPath;
     this.info = function() {
         return `${this.title}, by ${this.author}, has ${this.pages} pages, and is ${this.isRead}`
     };
@@ -16,18 +17,18 @@ function addBookToLibrary(bookToAdd) {
 
 // Add default books to myLibary
 
-let book1 = new Book("Emma", "Jane Austen", 495, "read");
-let book2 = new Book("Emma", "Pride and Prejudice", 274, "not read");
-let book3 = new Book("Heidi Shamsuddin", "Nusantara, A Sea of Tales", 223, "not read");
-let book4 = new Book("The Psychology of Slaying Dragons", "Kristian Bell", 282, "not read");
-let book5 = new Book("L. Varty & L. H. Yim", "Hong Kong Slang", 115, "not read");
-let book6 = new Book("Levy Rozman", "How To Win At Chess", 361, "read");
-let book7 = new Book("Fyodor Dostoevsky", "Crime and Punishment", 642, "read");
-let book8 = new Book("Gabot Mate", "The Myth of Normal", 563, "read");
-let book9 = new Book("Alan Watts", "Nature, Man and Woman", 187, "not read");
-let book10 = new Book("Alex Hormozi", "$100m Offers", 231, "read");
-let book11 = new Book("Alex Hormozi", "$100m Leads", 398, "read");
-let book12 = new Book("Vadim Zealand", "Tufti The Priestess", 167, "read");
+let book1 = new Book("Emma", "Jane Austen", 495, "read", "img/books/emma");
+let book2 = new Book("Emma", "Pride and Prejudice", 274, "not read", "img/books/pride");
+let book3 = new Book("Heidi Shamsuddin", "Nusantara, A Sea of Tales", 223, "not read", "img/books/nusantara");
+let book4 = new Book("The Psychology of Slaying Dragons", "Kristian Bell", 282, "not read", "img/books/dragons");
+let book5 = new Book("L. Varty & L. H. Yim", "Hong Kong Slang", 115, "not read", "img/books/hkslang");
+let book6 = new Book("Levy Rozman", "How To Win At Chess", 361, "read", "img/books/chess");
+let book7 = new Book("Fyodor Dostoevsky", "Crime and Punishment", 642, "read", "img/books/crime");
+let book8 = new Book("Gabot Mate", "The Myth of Normal", 563, "read", "img/books/mythofnormal");
+let book9 = new Book("Alan Watts", "Nature, Man and Woman", 187, "not read", "img/books/nature");
+let book10 = new Book("Alex Hormozi", "$100m Offers", 231, "read", "img/books/100moffers");
+let book11 = new Book("Alex Hormozi", "$100m Leads", 398, "read", "img/books/100mleads");
+let book12 = new Book("Vadim Zealand", "Tufti The Priestess", 167, "read", "img/books/tufti");
 
 // Add default display books
 
